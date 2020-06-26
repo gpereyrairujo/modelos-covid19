419398d5d317c369c5425355ee80b29b8809ecd8
gpereyrairujo <pereyrairujo.gustavo@conicet.gov.ar>
2020-06-25 21:06:09 -0300

    actualización mapas

--- datos/mapa_bsas.py
+++ datos/mapa_bsas.py
@@ -3,11 +3,73 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 
+
+def mapa_municipios(datos_mapa, columna_datos, ruta_imagen, titulo='', leyenda_pie='', rotulos=True, leyenda_escala=True, maximo_escala_log=3, ancho_pugadas=7, alto_pulgadas=5):
+    columna_municipio = 'Municipio'
+    # calcular logaritmo para usar esos valores para la escala de color del mapa
+    columna_datos_log = 'datos_log'
+    datos_mapa[columna_datos_log] = np.log10(datos_mapa[columna_datos])
+    # paleta de colores, bordes, texto
+    paleta_colores = plt.cm.get_cmap('Blues')
+    paleta_colores.set_under('white')           # valores por debajo del mínimo (ceros)
+    color_fondo = 'white'
+    color_bordes = 'darkred'
+    grosor_bordes = 0.1
+    tamanio_rotulos=7
+    tamanio_titulo=9
+    tamanio_leyenda=6
+    # figura, tamaño, ejes
+    fig, ax = plt.subplots(1, figsize=(ancho_pugadas, alto_pulgadas))       # crear figura y asignar tamaño
+    plt.axis('equal')                                                       # mantener proporción latitud y longitud
+    ax.set_axis_off()                                                       # quitar ejes
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.margins(0.05,0.05)                                                  # margen alrededor del mapa
+    fig.patch.set_facecolor(color_fondo)                                    # color del fondo
+    # datos para el mapa
+    ax = datos_mapa.plot(
+        column=columna_datos_log,                                   # valores a usar para la escala de colores
+        cmap=paleta_colores, vmin=0, vmax=maximo_escala_log,        # escala de colores entre valores log 0 y 3 (entre 1 y 1000)
+        edgecolor=color_bordes, linewidth=grosor_bordes,            # bordes
+        ax=ax)
+    # rótulos para cada municipio
+    if(rotulos):
+        for idx, fila in datos_mapa.iterrows():
+            if(fila[columna_datos]>0):
+                plt.text(
+                    fila['Longitud'], fila['Latitud'],                                 # ubicación
+                    fila[columna_municipio]+'\n'+str(int(fila[columna_datos])),              # texto
+                    {'color':'black', 'fontsize':tamanio_rotulos, 'ha':'center', 'va':'center'},     # formato
+                    bbox=dict(boxstyle="round", facecolor='lightgrey', edgecolor='white', alpha=0.5, linewidth=grosor_bordes)           # cuadro de texto
+                )
+    # leyendas
+    izquierda, derecha = plt.xlim()     # límites del mapa en x
+    abajo, arriba = plt.ylim()          # límites del mapa en y
+    plt.text((izquierda+derecha)/2, arriba, titulo, {'color': 'black', 'fontsize': tamanio_titulo, 'ha': 'center', 'fontweight': 'bold'})
+    plt.text(izquierda, abajo, leyenda_pie, {'color': 'black', 'fontsize': tamanio_leyenda})
+    # leyenda rangos de colores
+    if(leyenda_escala):
+        for i in range(4):
+            valor = 10**i
+            color = paleta_colores(i/3)
+            posicion_y = abajo - (4-i) * (abajo-arriba)/20
+            posicion_x = derecha - (derecha-izquierda)/30
+            plt.text(
+                posicion_x, posicion_y,
+                str(valor),
+                {'color':'black', 'fontsize':tamanio_rotulos, 'ha':'right', 'va':'center'},
+                bbox=dict(boxstyle="round", facecolor=color, edgecolor=color_bordes, linewidth=0.1))
+    # guardar mapa
+    plt.savefig(ruta_imagen, facecolor=fig.get_facecolor(), bbox_inches = 'tight', pad_inches = 0.1)
+
+
+
+# mapas
+
 carpeta_origen = './'
 carpeta_destino = carpeta_origen
 archivo_datos_mapa = 'kml_municipios/municipios.shp'
 archivo_casos_municipios = 'datos_minsal_acumulados_municipios_bsas.csv'
-archivo_destino_imagen = 'mapa_casos_region.png'
 
 # leer datos mapa base
 ruta = carpeta_origen + archivo_datos_mapa
@@ -24,108 +86,24 @@ datos_casos_municipios = pd.read_csv(ruta)
 # unir la tabla de casos de municipios de la pcia de bs as y los datos del mapa base
 datos_mapa = datos_mapa.merge(datos_casos_municipios, left_on='NAM', right_on='Municipio', how='left')
 # calcular casos totales
-datos_mapa['total']=datos_mapa['Activo']+datos_mapa['Fallecido']+datos_mapa['Recuperado']
-# calcular logaritmo para usar esos valores para la escala de color del mapa
-datos_mapa['log_total'] = np.log10(datos_mapa['total'])
+datos_mapa['Total']=datos_mapa['Activo']+datos_mapa['Fallecido']+datos_mapa['Recuperado']
 # fecha de última actualización
 ultima_actualizacion = datos_mapa.ix[1, 'ultima_actualizacion']
 
 
-# mapas
-
-# paleta de colores
-paleta_colores = plt.cm.get_cmap('Blues')
-paleta_colores.set_under('white')                   # valores por debajo del mínimo en color blanco
-color_fondo = 'white'
-color_bordes = 'darkred'
-
-
-# mapa provincia
-# figura, tamaño, ejes
-fig, ax = plt.subplots(1, figsize=(4.3, 5))       # crear figura y asignar tamaño
-plt.axis('equal')                               # mantener proporción latitud y longitud
-ax.set_axis_off()                               # quitar ejes
-plt.gca().xaxis.set_major_locator(plt.NullLocator())
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-plt.margins(0.05,0.05)                          # margen alrededor del mapa
-fig.patch.set_facecolor(color_fondo)               # color del fondo
-# datos para el mapa
-ax = datos_mapa.plot(
-    column='log_total',                         # valores a usar para la escala de colores
-    cmap=paleta_colores, vmin=0, vmax=3,        # escala de colores entre valores log 0 y 3 (entre 1 y 1000)
-    edgecolor=color_bordes, linewidth=0.1,          # color de los bordes
-    ax=ax
-    )
-# leyendas
-izquierda, derecha = plt.xlim()     # límites del mapa en x
-abajo, arriba = plt.ylim()          # límites del mapa en y
-plt.text((izquierda+derecha)/2, arriba, 'Casos confirmados Pcia. de Bs. As.', {'color': 'black', 'fontsize': 10, 'ha': 'center', 'fontweight': 'bold'})
-plt.text(izquierda, abajo, 'Elaborado en base a datos del Ministerio de Salud del día '+ultima_actualizacion, {'color': 'black', 'fontsize': 6})
-# leyenda rangos de colores 
-for i in range(4):
-    valor = 10**i
-    color = paleta_colores(i/3)
-    posicion_y = abajo - (4-i) * (abajo-arriba)/20
-    posicion_x = derecha - (derecha-izquierda)/30
-    plt.text(
-        posicion_x, posicion_y,
-        str(valor),
-        {'color':'black', 'fontsize':7, 'ha':'right', 'va':'center'},
-        bbox=dict(boxstyle="round", facecolor=color, edgecolor=color_bordes, linewidth=0.1)
-    )
-# guardar y mostrar mapa
+# mapa 1: provincia bs as
+titulo = 'Casos confirmados de COVID-19 - '+ultima_actualizacion
+leyenda = 'Elaborado en base a datos abiertos del Ministerio de Salud'
 ruta_imagen = carpeta_destino + 'mapa_casos_provincia.png'
-plt.savefig(ruta_imagen, facecolor=fig.get_facecolor(), bbox_inches = 'tight', pad_inches = 0.1)
+# dibujar mapa
+mapa_municipios(datos_mapa, 'Total', ruta_imagen, titulo, leyenda, rotulos=False, leyenda_escala=True, maximo_escala_log=3, ancho_pugadas=4.3, alto_pulgadas=5)
 
-
-# mapa region centro-sudeste
-
+# mapa 2: region centro-sudeste
+titulo = 'Casos confirmados de COVID-19 - '+ultima_actualizacion
+leyenda = 'Elaborado en base a datos abiertos del Ministerio de Salud'
+ruta_imagen = carpeta_destino + 'mapa_casos_region.png'
 # filtrar por latitud y longitud
 datos_mapa = datos_mapa.loc[(datos_mapa['Latitud']<-35.7) & (datos_mapa['Longitud']>-60.8)]
-
 # dibujar mapa
-# figura, tamaño, ejes
-fig, ax = plt.subplots(1, figsize=(7, 5))       # crear figura y asignar tamaño
-plt.axis('equal')                               # mantener proporción latitud y longitud
-ax.set_axis_off()                               # quitar ejes
-plt.gca().xaxis.set_major_locator(plt.NullLocator())
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
-plt.margins(0.05,0.05)                          # margen alrededor del mapa
-fig.patch.set_facecolor(color_fondo)               # color del fondo
-# datos para el mapa
-ax = datos_mapa.plot(
-    column='log_total',                         # valores a usar para la escala de colores
-    cmap=paleta_colores, vmin=0, vmax=3,        # escala de colores entre valores log 0 y 3 (entre 1 y 1000)
-    edgecolor=color_bordes, linewidth=0.1,          # color de los bordes
-    ax=ax
-    )
-# rótulos para cada municipio
-for idx, fila in datos_mapa.iterrows():
-    if(fila['total']>0):
-        plt.text(
-            fila['Longitud'], fila['Latitud'],                                                      # ubicación
-            fila['Municipio']+'\n'+str(int(fila['total'])),                                         # texto
-            {'color':'black', 'fontsize':7, 'ha':'center', 'va':'center'},     # formato
-            bbox=dict(boxstyle="round", facecolor='lightgrey', edgecolor='white', alpha=0.5, linewidth=0.1)           # cuadro de texto
-        )
-# leyendas
-izquierda, derecha = plt.xlim()     # límites del mapa en x
-abajo, arriba = plt.ylim()          # límites del mapa en y
-plt.text((izquierda+derecha)/2, arriba, 'Casos confirmados en la región', {'color': 'black', 'fontsize': 10, 'ha': 'center', 'fontweight': 'bold'})
-plt.text(izquierda, abajo, 'Elaborado en base a datos del Ministerio de Salud del día '+ultima_actualizacion, {'color': 'black', 'fontsize': 6})
-# leyenda rangos de colores 
-for i in range(4):
-    valor = 10**i
-    color = paleta_colores(i/3)
-    posicion_y = abajo - (4-i) * (abajo-arriba)/20
-    posicion_x = derecha - (derecha-izquierda)/30
-    plt.text(
-        posicion_x, posicion_y,
-        str(valor),
-        {'color':'black', 'fontsize':7, 'ha':'right', 'va':'center'},
-        bbox=dict(boxstyle="round", facecolor=color, edgecolor=color_bordes, linewidth=0.1)
-    )
-# guardar y mostrar mapa
-ruta_imagen = carpeta_destino + archivo_destino_imagen
-plt.savefig(ruta_imagen, facecolor=fig.get_facecolor(), bbox_inches = 'tight', pad_inches = 0.1)
-#plt.show()
+mapa_municipios(datos_mapa, 'Total', ruta_imagen, titulo, leyenda, rotulos=True, leyenda_escala=True, maximo_escala_log=3, ancho_pugadas=7, alto_pulgadas=5)
+
